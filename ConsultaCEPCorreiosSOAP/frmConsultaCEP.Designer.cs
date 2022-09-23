4c109155a9860e4635330db014a98a6808a572d1
dsrocha01 <dsrocha01@gmail.com>
2022-09-22 22:56:58 -0300

    Exercicio 1
    Commit do Exercicio 1 da Disciplina APIs e WebServices.

---
+++ ConsultaCEPCorreiosSOAP/frmConsultaCEP.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace ConsultaCEPCorreiosSOAP
+{
+    partial class frmConsultaCEP
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lblAguarde = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(226, 62);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(150, 25);
+            this.btnConsultar.TabIndex = 0;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(15, 65);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(150, 20);
+            this.txtCEP.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite o CEP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Consulta CEP Correios SOAP ";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 134);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(360, 93);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.Visible = false;
+            // 
+            // lblAguarde
+            // 
+            this.lblAguarde.AutoSize = true;
+            this.lblAguarde.Location = new System.Drawing.Point(135, 108);
+            this.lblAguarde.Name = "lblAguarde";
+            this.lblAguarde.Size = new System.Drawing.Size(130, 13);
+            this.lblAguarde.TabIndex = 6;
+            this.lblAguarde.Text = "Consulta em andamento...";
+            this.lblAguarde.Visible = false;
+            // 
+            // frmConsultaCEP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 241);
+            this.Controls.Add(this.lblAguarde);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.btnConsultar);
+            this.Name = "frmConsultaCEP";
+            this.Text = "PUC Minas - AWS - Exercicio 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label lblAguarde;
+    }
+}
+
---
+++ ConsultaCEPCorreiosSOAP/frmConsultaCEP.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultaCEPCorreiosSOAP
+{
+    public partial class frmConsultaCEP : Form
+    {
+        public frmConsultaCEP()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            using(var ws = new WSCorreios.AtendeClienteClient())
+            {
+                try
+                {
+                    var cepDigitado = txtCEP.Text;
+                    
+                    lblAguarde.Visible = true;
+                    richTextBox1.Visible = false;
+
+                    if (cepDigitado.Trim() == "")
+                    {
+                        MessageBox.Show("Digite um CEP");
+                        richTextBox1.Visible = false;
+                        lblAguarde.Visible = false;
+                    }
+                    else
+                    {
+                        var resultado = ws.consultaCEP(cepDigitado.Trim());
+
+                        lblAguarde.Visible = false;
+                        richTextBox1.Text = "";
+                        richTextBox1.Visible = true;
+                        richTextBox1.Text += "Logradouro: " + resultado.end + Environment.NewLine;
+                        richTextBox1.Text += "Bairro: " + resultado.bairro + Environment.NewLine;
+                        richTextBox1.Text += "Cidade: " + resultado.cidade + Environment.NewLine;
+                        richTextBox1.Text += "Estado: " + resultado.uf;
+
+                    }
+                }
+                catch(Exception ex)
+                {
+                    lblAguarde.Visible = false;
+                    richTextBox1.Visible = false;
+                    MessageBox.Show("Erro ao consultar o CEP: " + ex.Message);
+                }
+                
+
+            }
+            
+
+        }
+    }
+}
